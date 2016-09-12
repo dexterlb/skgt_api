c2ca610e36cefcddd6a2a6569944ceb2c4db24f1
Angel Angelov <hextwoa@gmail.com>
2016-09-12 20:37:20 +0300

    parse schedule times

--- schedules/schedule.go
+++ schedules/schedule.go
@@ -1,7 +1,9 @@
 package schedules
 
 import (
+	"encoding/json"
 	"fmt"
+	"regexp"
 	"strconv"
 	"strings"
 
@@ -10,9 +12,12 @@ import (
 	"github.com/jbowtie/gokogiri/xml"
 )
 
-type Time [2]int
+type Time struct {
+	Hours   int
+	Minutes int
+}
 
-type Course []Time
+type Course []*Time
 
 type Schedule struct {
 	Type      string
@@ -125,7 +130,81 @@ func parseStops(scheduleDiv xml.Node) ([]int, error) {
 }
 
 func parseCourses(scheduleDiv xml.Node) ([]Course, error) {
-	return nil, nil
+	timeCells, err := scheduleDiv.Search(
+		`.//div[contains(@class, 'hours_cell')]/a`,
+	)
+	if err != nil {
+		return nil, fmt.Errorf("unable to find timetable cells", err)
+	}
+
+	courses := make([]Course, len(timeCells))
+	for i := range timeCells {
+		jsLink, ok := timeCells[i].Attributes()["onclick"]
+		if !ok {
+			return nil, fmt.Errorf("time link has no onclick attribute")
+		}
+
+		courses[i], err = parseCourse(jsLink.Value())
+		if err != nil {
+			return nil, fmt.Errorf("unable to parse course: %s", err)
+		}
+	}
+
+	return courses, nil
+}
+
+func parseCourse(jsCall string) (Course, error) {
+	// we shall now parse javascript code.
+	// here be dragons.
+
+	// a sample jsCall:
+	// Raz.exec ('show_course', ['9caca5ad9', '4,761,763,765,766,767,768,770,772,775,777,780,783,785,788,792,794,796,798,800,802,804,806,807']); return false;
+
+	// we need the '4,761,...' part.
+	groups := regexp.MustCompile(
+		`.+\[\'\w+\', \'([0-9, ]+)\'\].*`,
+	).FindStringSubmatch(jsCall)
+	if len(groups) < 2 {
+		return nil, fmt.Errorf("unable to find time list in this javascript code: %s", jsCall)
+	}
+
+	times := strings.Split(groups[1], ",")[1:]
+
+	course := make(Course, len(times))
+	var err error
+	for i := range times {
+		course[i], err = parseTime(times[i])
+		if err != nil {
+			return nil, fmt.Errorf("unable to parse time: %s", err)
+		}
+	}
+
+	return course, nil
+}
+
+func parseTime(skgtTime string) (*Time, error) {
+	if len(skgtTime) == 0 {
+		return nil, nil // empty string is valid - transport doesn't reach stop
+	}
+
+	// skgtTime is actually an integer: number of minutes since 00:00
+	minutes, err := strconv.Atoi(skgtTime)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Time{
+		Hours:   minutes / 60,
+		Minutes: minutes % 60,
+	}, nil
+}
+
+func (t *Time) MarshalJSON() ([]byte, error) {
+	if t == nil {
+		return json.Marshal(nil)
+	}
+
+	return json.Marshal(fmt.Sprintf("%02d:%02d", t.Hours, t.Minutes))
 }
 
 func transportType(transport common.Transport) string {
